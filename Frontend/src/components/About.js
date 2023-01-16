eb84fc7448ce2b1d9b48ad8928540e39b53a24e1
ShivaRK <shivarkcodes@gmail.com>
2023-01-16 19:45:16 +0530

    modified:   Frontend/src/components/About.js 	modified:   Frontend/src/components/Navbar.js

--- Frontend/src/components/About.js
+++ Frontend/src/components/About.js
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react'
-import NoteContext from '../context/notes/NoteContext'
+import React from 'react'
 
 export const About = () => {
-    const a = useContext(NoteContext)
   return (
-    <div>This is {a.name}</div>
+    <div className="container fs-4">
+        <b>iNotebook</b> is online notes app to store personal notes and passwords for their personal use. <br />
+        I am <i>ShivaRK</i> created iNotebook to kickstart my journey of MERN stack<br />
+        Used Technologies : MongoDB, Express JS, React JS, Node JS, Bootstrap
+      </div>
   )
 }
